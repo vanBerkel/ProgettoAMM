7869894a9475829270b1cca5fd3828377ad6e998
Pier <>
2014-09-15 13:48:00 +0200

    azzeramento

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
-$_SERVER['HTTP_HOST']='spano.sc.unica.it';
-echo $_SERVER['HTTP_HOST'];
+//$_SERVER['HTTP_HOST']='spano.sc.unica.it';
+//echo $_SERVER['HTTP_HOST'];
 
 /*?>
 <a href="spano.sc.unica.it/phpmyadmin">ciao </a>
--- php/view/cliente/header.php
+++
@@ -1,6 +0,0 @@
-<div id="header">
-<h1>Vivaio </h1>
-<?php
-    echo "<p> Benvenuto " . $_SESSION['logged'] . "</p>";
-?>
-</div>

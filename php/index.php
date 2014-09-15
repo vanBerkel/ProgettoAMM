630f2e0f082a50c777dde2e03af08426af72a66d
Vale <Vale@10.50.228.154>
2014-09-15 15:48:09 +0200

    date default

--- php/index.php
+++ php/index.php
@@ -1,11 +1,11 @@
 <?php
 session_start();//apre la sessione
+date_default_timezone_set("Europe/Rome");
 if ($_SERVER['HTTP_HOST']!="localhost"){
     include("dbclass.php");
-
 }
 else{
-include("dbclass_1.php");    
+    include("dbclass_1.php");    
 }
 include("controller/functions.php");
 include("controller/generalControl.php");

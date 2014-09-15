c953b4b8a6b15f73a3b309569a06c71faf779bcb
Vale <Vale@10.50.228.154>
2014-09-15 14:03:26 +0200

    pulizia index

--- php/index.php
+++ php/index.php
@@ -2,7 +2,7 @@
 session_start();//apre la sessione
 include("dbclass.php");
 include("controller/functions.php");
-include("controller/GeneralControl.php");
+include("controller/generalControl.php");
 include("view/master.php");
 
 
